d286130f3cdd299969ed8084f4f05900d4eb9b40
zhengjieWeb0520 <zhengjie0601@sina.com>
2018-09-02 12:07:58 +0800

    modify menu click

--- src/component/router/footer.js
+++ src/component/router/footer.js
@@ -2,13 +2,6 @@ import React, { Component } from 'react'
 import { Link, withRouter } from 'react-router-dom'
 import { getChildNode } from './../../common/common'
 
-import activityImg from './../../images/icon/acticity@2x.png'
-import participateImg from './../../images/icon/participate@2x.png'
-import myInfoImg from './../../images/icon/myinfo@2x.png'
-import activityActiveImg from './../../images/icon/acticity-active@2x.png'
-import participateActiveImg from './../../images/icon/participate-active@2x.png'
-import myInfoActiveImg from './../../images/icon/myinfo-active@2x.png'
-
 class Footer extends Component{
   componentDidMount(){
     let navListUl = document.querySelector("footer ul")
@@ -18,8 +11,10 @@ class Footer extends Component{
       navListLis.forEach((item, index)=>{
         item.classList.remove('menuActive')
       })
-      if(e.target.tagName === 'a'){
+      if(e.target.tagName === 'A'){
         e.target.parentNode.classList.add('menuActive')
+      }else if(e.target.tagName === 'li'){
+        e.target.classList.add('menuActive')
       }else {
         e.target.parentNode.parentNode.classList.add('menuActive')
       }
