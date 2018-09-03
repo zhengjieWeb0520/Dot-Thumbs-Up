11c52f328418cff64f6fecd986f1d979d8d93c33
zhengjieWeb0520 <zhengjie0601@sina.com>
2018-09-03 12:38:47 +0800

    activity index

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'antd-mobile/dist/antd-mobile.css'
 import './css/Reset.css';
 import './index.css';
 import Main from './component/main';
