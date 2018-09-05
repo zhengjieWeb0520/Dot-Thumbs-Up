335b1ef5451657402b3506dc0cd8c9e06254d5ce
wallet <918061733@qq.com>
2018-09-05 09:40:12 +0800

    add feild style

--- src/component/4-myInfo/components/chooseBankCard.js
+++ src/component/4-myInfo/components/chooseBankCard.js
@@ -11,58 +11,57 @@ class ChooseBankCard extends React.Component {
       <div className="chooseBankCard">
         <TopNavBar title="选择银行卡" rightContent={false}/>
         <div className="cardBox">
-        <div className="addCard">
-          <Link to={`/chooseBankCard/newBankCard`}>
-            新增银行卡
-          </Link>
-        </div>
-        <div className="card">
-          <List>
-            <SwipeAction
-              right={[
-                {
-                  text: 'delete',
-                  onPress: () => console.log('delete'),
-                  style: { backgroundColor: '#fff', color: 'red' },
-                },
-              ]}
-              onOpen={() => console.log('global open')}
-              onClose={() => console.log('global close')}
-            >
-              <Item
-                thumb={require("../../../images/myInfo/icon_gongshang.png")}
-                multipleLine
-                onClick={() => {}}
+          <div className="card">
+            <List>
+              <SwipeAction
+                right={[
+                  {
+                    text: 'delete',
+                    onPress: () => console.log('delete'),
+                    style: { backgroundColor: '#fff', color: 'red' },
+                  },
+                ]}
+                onOpen={() => console.log('global open')}
+                onClose={() => console.log('global close')}
               >
-                中国工商银行
-                <Brief>6222****5077</Brief>
-                <span className="bankBranch">浦口支行</span>
-              </Item>
-            </SwipeAction>
-            <SwipeAction
-              right={[
-                {
-                  text: 'delete',
-                  onPress: () => console.log('delete'),
-                  style: { backgroundColor: '#fff', color: 'red' },
-                },
-              ]}
-              onOpen={() => console.log('global open')}
-              onClose={() => console.log('global close')}
-            >
-              <Item
-                thumb={require("../../../images/myInfo/icon_jianshe.png")}
-                multipleLine
-                onClick={() => {}}
+                <Item
+                  thumb={require("../../../images/myInfo/icon_gongshang.png")}
+                  multipleLine
+                  onClick={() => {}}
+                >
+                  中国工商银行
+                  <Brief>6222****5077</Brief>
+                  <span className="bankBranch">浦口支行</span>
+                </Item>
+              </SwipeAction>
+              <SwipeAction
+                right={[
+                  {
+                    text: 'delete',
+                    onPress: () => console.log('delete'),
+                    style: { backgroundColor: '#fff', color: 'red' },
+                  },
+                ]}
+                onOpen={() => console.log('global open')}
+                onClose={() => console.log('global close')}
               >
-                中国建设银行
-                <Brief>6222****5077</Brief>
-                <span className="bankBranch">青浦支行</span>
-              </Item>
-            </SwipeAction>
-          </List>
-        </div>
-        
+                <Item
+                  thumb={require("../../../images/myInfo/icon_jianshe.png")}
+                  multipleLine
+                  onClick={() => {}}
+                >
+                  中国建设银行
+                  <Brief>6222****5077</Brief>
+                  <span className="bankBranch">青浦支行</span>
+                </Item>
+              </SwipeAction>
+            </List>
+          </div>
+          <div className="addCard">
+            <Link to={`/chooseBankCard/newBankCard`}>
+              新增银行卡
+            </Link>
+          </div>
         </div>
       </div>
     )
