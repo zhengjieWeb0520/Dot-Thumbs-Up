ef6dd8cd5f3e959fd218dabe5c183085a5fba26e
zhengjieWeb0520 <zhengjie0601@sina.com>
2018-09-05 09:45:34 +0800

    activity

--- src/component/1-activity/activity.js
+++ src/component/1-activity/activity.js
@@ -1,12 +1,8 @@
 import React from 'react'
-import './../../css/activity.css'
+import './activity.scss'
 import { SearchBar } from 'antd-mobile'
-import positionInfo from './../../images/icon/position@2x.png'
-import lollipop from './../../images/icon/lollipop@2x.png'
-import bicycle from './../../images/icon/bicycle@2x.png'
-import babyball from './../../images/icon/babyball@2x.png'
-import hotail from './../../images/icon/hotail@2x.png'
-import aerospace from './../../images/icon/aerospace@2x.png'
+import { Select } from 'antd'
+const Option = Select.Option;
 
 // 活动组件
 class Activity extends React.Component{
@@ -32,17 +28,6 @@ class Activity extends React.Component{
       }
       
      }, false);
-         // 获取地理位置
-    // fetch("http://restapi.amap.com/v3/ip?key=4edfca722186816775170b7f8915eb5e").then((res)=>{
-    //   if(res.ok){
-    //     res.text().then((data)=>{
-    //       const detail=JSON.parse(data)
-    //       console.log(detail)
-    //     })
-    //   }
-    // }).catch((res)=>{
-    //   console.log(res.status);
-    // });
   }
   render(){
     let iframeStyle = {
@@ -58,81 +43,87 @@ class Activity extends React.Component{
           src="https://apis.map.qq.com/tools/geolocation?key=OB4BZ-D4W3U-B7VVO-4PJWW-6TKDJ-WPB77&referer=myapp">
         </iframe>
         <div className='activityCondition'>
+          <div className='activityKb'></div>
           <div className='activityHeader'>
-            <div><img src={positionInfo} alt=""/><span>{this.state.address}</span></div>
+            <div><i className='positionIcon'></i><span>{this.state.address}</span></div>
             <div><SearchBar placeholder="输入商家或活动关键字" maxLength={8} /></div>
           </div>
           <div className='activityLevel'>
             <div className='activityLevel1'>
               <ul>
-                <li>
-                  <img src={lollipop} /> 
+                <li id='foodLevel'>
+                  <i></i> 
                   <p>享美食</p>
                 </li>
-                <li>
-                  <img src={bicycle} />
+                <li id='lifeLevel'>
+                  <i></i>
                   <p>惠生活</p>
                 </li>
-                <li>
-                  <img src={babyball} />
+                <li id='enjoyLevel'>
+                  <i></i>
                   <p>爱玩乐</p>                 
                 </li>
-                <li>
-                  <img src={hotail} />
+                <li id='HotelLevel'>
+                  <i></i>
                   <p>住酒店</p>   
                 </li>
-                <li>
-                  <img src={aerospace} />
+                <li id='allLevel'>
+                  <i></i>
                   <p>全部</p>   
                 </li>
               </ul>
             </div>
             <div className='activityLevel2'>
-
+              <div>
+                <span>
+                  热门
+                </span>
+                <span>
+                  小吃快餐
+                </span>
+                <span>
+                  甜点饮品
+                </span>
+                <span>
+                  火锅烧烤
+                </span>
+                <span>
+                  日韩料理
+                </span>
+                <span>
+                  炸鸡可乐
+                </span>
+              </div>
+            </div>
+            <div className='activityKbGray'>
+            </div>
+            <div className='activityLevel3'>
+              <ul>
+                <li>
+                  <select>
+                    <option>综合排序</option>
+                    <option>好评优先</option>
+                    <option>销量最高</option>
+                    <option>点赞最多</option>
+                  </select>
+                </li>
+                <li>
+                  <span>距离最近</span>
+                </li>
+                <li>
+                  <select>
+                    <option>金牌商家</option>
+                    <option>银牌商家</option>
+                    <option>铜牌商家</option>
+                  </select>
+                </li>
+              </ul>
             </div>
           </div>   
         </div>
         <div className='activityContent'>
           <div>
-          <p style={{marginTop: '20px'}}>
-            段时间看大叔将搭建了圣诞节爱上了
-          </p>    
-          <p style={{marginTop: '20px'}}>
-            段时间看大叔将搭建了圣诞节爱上了
-          </p>
-          <p style={{marginTop: '20px'}}>
-            段时间看大叔将搭建了圣诞节爱上了
-          </p>
-          <p style={{marginTop: '20px'}}>
-            段时间看大叔将搭建了圣诞节爱上了
-          </p>
-          <p style={{marginTop: '20px'}}>
-            段时间看大叔将搭建了圣诞节爱上了
-          </p>
-          <p style={{marginTop: '20px'}}>
-            段时间看大叔将搭建了圣诞节爱上了
-          </p>
-          <p style={{marginTop: '20px'}}>
-            段时间看大叔将搭建了圣诞节爱上了
-          </p>
-          <p style={{marginTop: '20px'}}>
-            段时间看大叔将搭建了圣诞节爱上了
-          </p>
-          <p style={{marginTop: '20px'}}>
-            段时间看大叔将搭建了圣诞节爱上了
-          </p>
-          <p style={{marginTop: '20px'}}>
-            段时间看大叔将搭建了圣诞节爱上了
-          </p>
-          <p style={{marginTop: '20px'}}>
-            段时间看大叔将搭建了圣诞节爱上了
-          </p>
-          <p style={{marginTop: '20px'}}>
-            段时间看大叔将搭建了圣诞节爱上了
-          </p>
-          <p style={{marginTop: '20px'}}>
-            段时间看大叔将搭建了圣诞节爱上了
-          </p>
+
           </div>
         </div>
       </div>
