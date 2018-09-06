0261e02bc842fe56944ffc2c4c1e18dd7976efc2
zhengjieWeb0520 <zhengjie0601@sina.com>
2018-09-06 14:08:25 +0800

    participate

--- src/component/1-activity/activity.js
+++ src/component/1-activity/activity.js
@@ -1,16 +1,21 @@
 import React from 'react'
 import './activity.scss'
-import { SearchBar } from 'antd-mobile'
-import { Select } from 'antd'
-const Option = Select.Option;
+import { SearchBar, Popover } from 'antd-mobile'
+import { getChildNode } from './../../utils/utils'
+import goldmedal from './../../images/activity/goldmedal@2x.png'
 
+const Item = Popover.Item;
 // 活动组件
 class Activity extends React.Component{
   constructor(props){
     super(props)
     this.loc = ''
     this.state= {
+      visible: false, //显隐状态
+      merchantVisible: false, //显隐状态
       TmapFlag: true, //只获取一次 
+      rankSelect: '综合排序', //综合排序
+      merchantSelect: '金牌商家', //商家
       address: ''
     }
   }
@@ -29,6 +34,39 @@ class Activity extends React.Component{
       
      }, false);
   }
+  componentDidMount(){
+    let navListUl = document.querySelector(".activityLevel1 ul")
+    let navListLis = getChildNode(navListUl)
+    //点击menu切换样式
+    navListUl.addEventListener('click', function(e){
+      navListLis.forEach((item, index)=>{
+        item.classList.remove('level1Active')
+      })
+      console.log(e)
+      if(e.target.tagName === 'I' || e.target.tagName === 'P'){
+        e.target.parentNode.classList.add('level1Active')
+      }else {
+        e.target.classList.add('level1Active')
+      }
+    }, false)
+  }
+  //排序选择
+  onSelect(node){
+    this.setState({
+      visible: false,
+      rankSelect: node.props.children
+    })
+  }
+  //商家选择
+  onMerchantSelect(node){
+    let iconDom = document.querySelector('#merchant')
+    iconDom.classList.remove(iconDom.className)
+    iconDom.classList.add(node.props.value)
+    this.setState({
+      merchantVisible: false,
+      merchantSelect: node.props.children
+    })
+  }
   render(){
     let iframeStyle = {
       width: '0',
@@ -38,7 +76,7 @@ class Activity extends React.Component{
       scrolling: 'no'
     }
     return(
-      <div id='Activity'>
+      <div id='Activity' className='acticityCommon'>
         <iframe id="geoPage" style={iframeStyle}
           src="https://apis.map.qq.com/tools/geolocation?key=OB4BZ-D4W3U-B7VVO-4PJWW-6TKDJ-WPB77&referer=myapp">
         </iframe>
@@ -51,7 +89,7 @@ class Activity extends React.Component{
           <div className='activityLevel'>
             <div className='activityLevel1'>
               <ul>
-                <li id='foodLevel'>
+                <li id='foodLevel' className='level1Active'>
                   <i></i> 
                   <p>享美食</p>
                 </li>
@@ -100,22 +138,71 @@ class Activity extends React.Component{
             <div className='activityLevel3'>
               <ul>
                 <li>
-                  <select>
-                    <option>综合排序</option>
-                    <option>好评优先</option>
-                    <option>销量最高</option>
-                    <option>点赞最多</option>
-                  </select>
+                    {this.state.rankSelect}
+                    <Popover
+                    mask={false}
+                    overlayClassName="fortest"
+                    overlayStyle={{color: 'currentColor'}}
+                    visible={this.state.visible}
+                    overlay={[
+                      (<Item key="1" value="scan" data-seed="logId">综合排序</Item>),
+                      (<Item key="2" value="scan" data-seed="logId">好评优先</Item>),
+                      (<Item key="3" value="scan" data-seed="logId">销量最高</Item>),
+                      (<Item key="4" value="scan" data-seed="logId">点赞最多</Item>),
+                    ]}
+                    align={{
+                      overflow: {adjustY: 0, adjustX: 0},
+                      offset: [0, 0],
+                    }}
+                    onVisibleChange={this.handleVisibleChange}
+                    onSelect={this.onSelect.bind(this)}
+                  >
+                    <div style={{
+                      position: 'absolute',
+                      right: '5%',
+                      top: '46%',
+                      transform: 'translateY(-50%)'
+                    }}
+                    >
+                      
+                      <i className='downArrow'></i>
+                    </div>
+                  </Popover>
                 </li>
                 <li>
                   <span>距离最近</span>
                 </li>
                 <li>
-                  <select>
-                    <option>金牌商家</option>
-                    <option>银牌商家</option>
-                    <option>铜牌商家</option>
-                  </select>
+                  <i id='merchant' className='goldmedal'></i>
+                {this.state.merchantSelect}
+                    <Popover
+                    mask={false}
+                    overlayClassName="fortest"
+                    overlayStyle={{color: 'currentColor'}}
+                    visible={this.state.merchantVisible}
+                    overlay={[
+                      (<Item key="6" className='goldmedal' value="goldmedal" icon={<img src={goldmedal}/>} data-seed="logId">金牌商家</Item>),
+                      (<Item key="7" className='silvermedal' value="silvermedal" icon={<img src={goldmedal}/>} data-seed="logId">银牌商家</Item>),
+                      (<Item key="8" className='bronzemedal' value="bronzemedal" icon={<img src={goldmedal}/>} data-seed="logId">铜牌商家</Item>),
+                    ]}
+                    align={{
+                      overflow: {adjustY: 0, adjustX: 0},
+                      offset: [0, 0],
+                    }}
+                    onVisibleChange={this.handleVisibleChange}
+                    onSelect={this.onMerchantSelect.bind(this)}
+                  >
+                    <div style={{
+                      position: 'absolute',
+                      right: '20%',
+                      top: '46%',
+                      transform: 'translateY(-50%)'
+                    }}
+                    >
+                      
+                      <i className='downArrow'></i>
+                    </div>
+                  </Popover>
                 </li>
               </ul>
             </div>
